e943eebd1f707157ab66e7b4f132c2d82ea3c3da
Lucas Vasquez <lucas.vasquezfc@gmail.com>
2023-12-06 11:28:07 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -41,3 +41,22 @@ function ativarPergunta(event) {
 perguntas.forEach((pergunta) => {
   pergunta.addEventListener('click', ativarPergunta);
 });
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width:  900px)').matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
